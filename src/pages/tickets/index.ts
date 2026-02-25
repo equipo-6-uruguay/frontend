6febd2449eaf17b9c8dd89ec30e0d9f7d1436d7c
Nahuel Lemes <nahulem@gmail.com>
2026-02-25 13:19:09 -0300

    refactor: remove unused barrel files from pages directories
    Deleted empty/unused index.ts files from:
    - src/pages/tickets/
    - src/pages/notifications/
    - src/pages/auth/
    - src/pages/assignments/
    
    AppRouter imports directly from the page components.

--- src/pages/tickets/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TicketList } from './TicketList';
-export { default as CreateTicket } from './CreateTicket';
-export { default as TicketDetail } from './TicketDetail';
-export { default as TicketItem } from '../../components/tickets/TicketItem';
-export { default as TicketForm } from '../../components/tickets/TicketForm';
